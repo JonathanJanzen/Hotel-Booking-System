aefb47c2dcce2c0bd71dd1193bc0827b929ce23a
Jonathan Janzen <jjanzen62@gmail.com>
2022-06-09 17:43:03 -0400

    Numerical values added for enum

--- HotelBookingSystem/src/HotelBookingSystem/RoomType.java
+++ HotelBookingSystem/src/HotelBookingSystem/RoomType.java
@@ -7,8 +7,26 @@ package HotelBookingSystem;
  *
  */
 public enum RoomType {
-	Double,
-	Twin,
-	Queen,
-	King
+	DOUBLE(0),
+	TWIN(1),
+	QUEEN(2),
+	KING(3);
+	
+	private final int value;
+	
+	/**
+	 * Private constructor, used to define the numerical value of each item.
+	 * @param _value Numerical value to be assigned to the enumerated type.
+	 */
+	private RoomType(int _value) {
+		this.value = _value;
+	}
+	
+	/**
+	 * Getter for the value of the enumerated class (used for indexing the Rooms list)
+	 * @return Integer value that corresponds to the type.
+	 */
+	public int getValue() {
+		return this.value;
+	}
 }
